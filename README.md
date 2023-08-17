11b65b240bbe99400df0525beb603ef58f6f8764
Ariel Rabinovich <airabinovich@gmail.com>
2023-08-17 12:06:55 +0200

    added readme and swagger documentation

---
+++ README.md
@@ -0,0 +1,5 @@
+# Node.js REST API with Express and MongoDB
+
+This project implements the learnings from [this udemy course](https://www.udemy.com/course/nodejs-rest-api-jwt-security-mongodb-complete-guide/)
+
+My certificate can be found [here](https://ude.my/UC-cd005099-5691-4999-ae63-4bbd96886e47)
--- app.js
+++ app.js
@@ -2,13 +2,16 @@ const express = require("express")
 const dotenv = require("dotenv")
 const productRouter = require('./api/productRouter');
 const userRouter = require('./api/userRouter');
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
 
 const dbConnection = require("./database/connection")
 
 dotenv.config()
 const app = express()
-//DB connection
 
+//DB connection
 dbConnection()
 
 app.use(express.json())
@@ -16,6 +19,11 @@ app.use(express.urlencoded())
 
 app.use("/api/v1/product", productRouter)
 app.use("/api/v1/user", userRouter)
+
+// API Documentation
+if (process.env.NODE_ENV !== "production") {
+    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+}
 
 const PORT = process.env.PORT || 8080
 
